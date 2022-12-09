a8fdfec4726f084c92c5bfd2edbfcb80f83ae864
milemotigo <mtigo3617.com>
2022-12-09 08:38:07 -0800

    good

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
-#include <stdio.h>                                                                                    
-
-int main(void) {                                                                                      
-
-	                                                                                                      
-
-	char c;                                                                                               
-        int i;
-        long int li;
-        long long int lli;
-        float f;
-	printf("size of a char: %ld byte(s).\n",(long unsigned int)sizeof(c));
-        printf("size of an int: %ld byte(s).\n",(long unsigned int)sizeof(i));
-        printf("size of a long int: %ld byte(s).\n",(long unsigned int)sizeof(li));
-        printf("size of a long long int: %ld byte(s).\n",(long unsigned int)sizeof(lli));
-        printf("size of a float: %ld byte(s).\n",(long unsigned int)sizeof(f));
-
-	return 0;                                                                                             
-
-}        
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ * */
+int main(void) {
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("size of a char: %ld byte(s)\n",(long unsigned int)sizeof(a));
+	printf("size of an int: %ld byte(s)\n",(long unsigned int)sizeof(b));
+	printf("size of a long int: %ld byte(s)\n",(long unsigned int)sizeof(c);
+			printf("size of a long long int: %ld byte(s).\n",(long unsigned int)sizeof(d);
+				printf("size of a float: %ld byte(s)\n",(long unsigned int)sizeof(e));
+				return (0);
+				}
