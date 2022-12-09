4e7cd84133d1e7ca6211eabd8ee47c854bf4befb
milemotigo <mtigo3617.com>
2022-12-09 12:12:42 -0800

    betty will u marry me

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
