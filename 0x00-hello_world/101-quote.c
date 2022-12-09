30f52e5087207cb25602ae896473565dba2d52a3
milemotigo <mtigo3617.com>
2022-12-09 12:38:18 -0800

    my betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
-int main (void)
-{
-	
-fprintf(stderr %s "and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n);
-return (0);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+fprintf("and that piece of art is useful\"- Dora Korpar, 2015-10-19"\n);
+return (1);
 }
