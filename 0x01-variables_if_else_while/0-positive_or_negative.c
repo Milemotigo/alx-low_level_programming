d32e65cdde8eb21547d1d44b7b0c5e9e1d4b2b62
milemotigo <mtigo3617.com>
2022-12-09 23:03:44 -0800

    my beby

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,12 +4,18 @@
 /*More headers goes there*/
 
 /*betty style doc for function main goes there*/
+/**
+ * main - Entry point
+ *
+ *Return:Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
