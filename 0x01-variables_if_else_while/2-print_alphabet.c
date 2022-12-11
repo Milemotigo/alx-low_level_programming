f60521054a3ddc40e281c42b3d342d365aeb9125
milemotigo <mtigo3617.com>
2022-12-10 22:27:30 -0800

    myf

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,2 +1,18 @@
-#include <stdio.h>
-#include <>
+#include <stdlib.h>
+/**
+ * main - This program will assign a random number to the variable n
+ * Return: Always 0 (Success)
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+	int lx = 'a';
+	while(lx <='z')
+		putchar("lx \n");
+	lx++;
+return (0);
+}
