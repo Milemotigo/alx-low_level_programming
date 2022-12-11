bc5d4297a38020707bfebada190a11e55b982b5c
milemotigo <mtigo3617.com>
2022-12-11 14:25:02 -0800

    mm

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,20 +2,21 @@
 /**
  * main - betty am here today
  *
- * return:always 0(Success)
+ *Return:always 0(Success)
  *
  */
 int main(void)
 {
-	int i = 97;
-	while (i < 123)
-	{
-		if(i != 101 && i != 113)
-		{
-			putchar(i);
-		}
-		i++;
-	}
-	putchar(10);
-	return (0);
+int i = 97;
+
+while (i < 123)
+{
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
+i++;
+}
+putchar(10);
+return (0);
 }
