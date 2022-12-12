3f2c9ba5adbc2de4f99ce745608656f8d461f9b4
milemotigo <mtigo3617.com>
2022-12-11 23:26:03 -0800

    ggoo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - good morning
+ *
+ * Return:Always return 0 (Success)
+ */
+int main(void)
+{
+	int i,j,k;
+
+	i = 48;
+	j = 48;
+	k = 48;
+
+	while (i < 58)
+	{
+		j= i + 1;
+		while (j < 58)
+			k = j + i;
+		{
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if(i < 55 || j < 56 || k < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+
+			}
+			j++;
+
+		}
+
+		i++;
+
+	}
+	putchar(10);
+	return (0);
+}
+
