42244de7604f9c883b68deb0a9da884a730144f9
milemotigo <mtigo3617.com>
2022-12-12 05:07:11 -0800

    my life

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,26 @@
  */
 int main(void)
 {
-	int i, j, k, m;
+	int i, j;
 
-	i = 48;
+	i= 48;
+	j = 48;
 	while (i < 58)
 	{
-		j = 48;
+		j = i + 1;
 		while (j < 58)
 		{
-			m = j + i;
-			k = i;
-			while (k < 58)
+			putchar(i);
+			putchar(j);
+			if (i < 56 || j < 57)
 			{
-				while (m < 58)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(m);
-					if (i < 57 || j < 56 || k < 57 || m < 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
-					m++;
-				}
-				m = 48;
-				k++;
-				{
-					j++;
-				}
-				i++;
-				{
-					putchar(10);
-					return (0);
-				}
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
