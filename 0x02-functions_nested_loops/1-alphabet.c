87249d65a61ccf117476c9a5446b43cd7f9a5467
milemotigo <mtigo3617.com>
2022-12-13 20:20:56 -0800

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - prints the alphabet, in lowercase, followed by a new line
- * Return:always 0.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ * Return:always 0
  */
 void print_alphabet(void)
 {
 	char q;
 
-	for (q = 'a'; q <='z'; q++)
+	for (q = '97'; q <= '122'; q++)
 	{
 		_putchar('q');
 		q++;
 	}
-	   _putchar('\n');
+	_putchar('\n');
 }
