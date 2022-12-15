cb9ca763d10c95ac7d9d08ed397d609dca58ec4a
milemotigo <mtigo3617.com>
2022-12-15 02:50:59 -0800

    monday

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,34 @@
 void jack_bauer(void)
 {
 	int i, j;
-	i = 0;
-	while (i < 24)
+
+	for (i = 0; i < 24; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else if (i > 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else if (j > 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }
