b4ddc069ec8fe14dd6aa807293db21903c9f1ad5
milemotigo <mtigo3617.com>
2022-12-14 22:04:09 -0800

    mu

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,26 +3,26 @@
  * print_sign - prints a sign of a number
  *
  * @c:The character to be checked
- *
+ *n: this is the variable declared
  * Return: Returns 1 and prints + if n is greater than zero
  * Return: 0 and prints 0 if n is zero
  * Return: -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
-
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
+		return (1);
 	}
-	return (1);
-	if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(45);
+		_putchar('0');
+		return (0);
 	}
-	return (-1);
+	else
 	{
-		_putchar(48);
+		_putchar('-');
+		return (-1);
 	}
-	return (0);
-}
+}		
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,6 @@
+#include "main.h"
+/**
+ * int_abs:computes the absolute value of an integer.
+ *
+ * Return:
+
