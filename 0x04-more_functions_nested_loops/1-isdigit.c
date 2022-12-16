732d02647748d6464e3671a5591c483a3164729a
milemotigo <mtigo3617.com>
2022-12-16 04:49:22 -0800

    is lower

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit-a digit number
+ * @c:character to check
+ * Return: 0 or 1;
+ */
+int _isdigit(int c)
+{
+	if(c >= '1'; && c <= '9')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplyb two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return the addition of two integers
+ */
+int mul(int a, int b)
+{
+	int mul;
+	mul = a * b;
+	_putchar("%d", a * b);
+}
