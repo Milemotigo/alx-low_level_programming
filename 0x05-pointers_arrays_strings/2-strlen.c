ea35c67bb1025027caa7888bb4b6efd94a79db74
milemotigo <mtigo3617.com>
2022-12-19 17:17:59 -0800

    variable pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: variable of the string
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++)
+		a++;
+}
