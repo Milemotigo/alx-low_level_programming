1e056d8d239362ba1d9efb447a1065f99712e148
milemotigo <mtigo3617.com>
2022-12-20 08:13:43 -0800

    string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,16 +3,16 @@
 /**
  * print_rev - function that print a string
  * @s: the string
- * Return 0;
+ * Return: 0
  */
 void print_rev(char *s)
 {
 	int a = 0;
-	while (s[a] != '\0');
-	{
-		_putchar(s[a]);
-		a--;
-	}
+
+	while (s[a] != '\0')
+		a++;
+	for (a = a - 1; a <= 0; a--)
+	_putchar(s[a]);
 _putchar('\n');
 }
 
