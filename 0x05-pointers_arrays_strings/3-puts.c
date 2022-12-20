a0b984118a50760f1222f9677ebdddf76787cba7
milemotigo <mtigo3617.com>
2022-12-20 01:38:03 -0800

    string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts - a funcrtion that prints a string
+ * _puts - a function that prints a string
  * @str: the string
  * Return: 0;
  */
 void _puts(char *str)
 {
-	int j = 0;
+	int a = 0;
 
-	while (str[j] != '\0')
+	while (str[a] != '\0')
 	{
-		_putchar(str[j]);
-		j++;
+		_putchar(str[a]);
+		a++;
 	}
 }
