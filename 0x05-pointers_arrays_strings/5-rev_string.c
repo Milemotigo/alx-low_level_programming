6f0733e849c654c8f1750ec5f9be5b68d264ed67
milemotigo <mtigo3617.com>
2022-12-21 03:45:37 -0800

    mm

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,20 +1,23 @@
-#include "main.h"
-/**
- * rev_string - A funtion that reverses a string
- *
- * @s: string
- *
- */
-void rev_string(char *s)
-{
-        int l;
-        int i;
-        char ch;
-        while (l != '\0')
-                l++;
-        for (i = 0; i < l / 2; i++);
-        ch  = s[i];
-        s[i] = s[l -  1 - i];
-        s[l -  1 - i] = ch;
-        _putchar(%s, 's');
-}
+#include "main.h" 
+ /** 
+  * rev_string - reverses a string 
+  * _putchar - print each character 
+  * @s: char to check 
+  * 
+  * Description: This will reverse a string 
+  * Return: 0 is success 
+  */ 
+ void rev_string(char *s) 
+ {
+	 char ch;
+	 int i =0;
+	 int l;
+
+	 while (l != '0')
+		 l++;
+	 for (i = 0; i < l / 2; i++)
+	 {
+		 ch = s[i];
+		 s[i] = s[l - 1 - i];
+		 s[l - 1 - i] = ch;
+	 }
