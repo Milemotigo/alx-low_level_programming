a0d1035aa80014039ef39b04a58fe2fdf36a37ba
milemotigo <mtigo3617.com>
2022-12-22 14:44:01 -0800

    hh

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,23 +4,21 @@
  * rev_string - reverses a string
  * _putchar - print each character
  * @s: char to check
- *
  * Description: This will reverse a string
  * Return: 0 is success
  */
 void rev_string(char *s)
 {
-	int i, l, j;
-	
+	int i=0, len = 0, ch;
+
 	while (s[i] != '\0')
-        {
-                i++;
-                l++;
-	}
-        for (i = 0; i < l/2; i++)
 	{
-		j = s[i];
-		s[i] = s[l - 1 - i];
-		s[l - 1 -i] = j;
+		len++;
+	}
+	for(i < (len)/ 2; i++;)
+	{
+		ch = s[i];
+		s[i] = s[len - 1 -i];
+		s[len - 1 - i] = ch;
 	}
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+
+for (n=0; n<5; n++)
+
+	printf("%d, ", a[n]);
+}
