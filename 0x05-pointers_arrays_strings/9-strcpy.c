0e28d8e5e3dcc77eae4c008e42db2d594ec42a72
milemotigo <mtigo3617.com>
2022-12-23 10:24:22 -0800

    my way

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string
+ * @dest: string to recieve a copy
+ * @src: string copied
+ * Return: success 0;
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	dest[a] = '\0';
+	return (0);
+}
