d44bbaeb9779e2e05f39cb04f2aa95286789ca04
milemotigo <mtigo3617.com>
2022-12-23 03:06:46 -0800

    sui wallet

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,23 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * print_array - function that print n element of array
+ * @n: int check
+ * @a: int check
+ * Return: 0
+ */
 void print_array(int *a, int n)
 {
+	int i;
 
-for (n=0; n<5; n++)
-
-	printf("%d, ", a[n]);
+	for (i = 0; i < n; i++)
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	printf("\n");
 }
