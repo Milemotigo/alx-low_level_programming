7a51f7db44916d8440a2896d4e37a432ee614f59
milemotigo <mtigo3617.com>
2023-01-03 17:24:06 -0800

    jjgg

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - the function that locates the first occurance of a string
+ * @s: string
+ * @accept: input
+ * Return: Returns a pointer to the byte in s & Null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
+
