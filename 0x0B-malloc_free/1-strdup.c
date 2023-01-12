05b0bb19b95f40b09a8a0ded4b9b5d6ab645a1fc
milemotigo <mtigo3617.com>
2023-01-12 04:38:13 -0800

    gd

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,45 +1,25 @@
 #include "main.h"
-#include<stdlib.h>
-#include <string.h> 
-#include <stddef.h>
+#include <stdlib.h>
+
 /**
- * _strdup - a function that returns a pointer to a newly allocated space in memory
- * @str: the src
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ *
  * Return: Returns NULL
  */
-int _strlen(char *t)
 char *_strdup(char *str)
 {
+	char *dest;
+	unsigned int x, y;
+
 	if (str == NULL)
 		return (NULL);
-	int len;
-	len = 0;
-	while (*t != '\0')
-	{
-		len = len + 1;
-		t = t + 1;
-	}
-	return (t);
-
-char *_strdup(char *str)
-
-	int len = 0;
-	char *saved;
-	char *dest;
-	len = _strlen(str);
-	dest = malloc((len + 1) * sizeof(char));
+	for (x = 0; str[x] != '\0'; x++)
+		;
+	dest = malloc(sizeof(char) * (x + 1));
 	if (dest == NULL)
-	{
 		return (NULL);
-	}
-	 char *_strcpy(char *dest, char *str)
-	saved = t;
-	while(*str != '\0')
-	{
-		*dest = *str;
-		dest = dest + 1;
-		str = str + 1;
-	}
-	*dest = '\0'
-	return (saved);
+	for (y = 0; y < x; y++)
+		dest[y] = str[y];
+	return (dest);
 }
