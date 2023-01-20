9c30ef3ed13c2bbe148958d87b1cb22da1ce8804
milemotigo <mtigo3617.com>
2023-01-19 23:46:13 -0800

    args

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...) 
+{
+	unsigned int i;
+	va_list args;
+	va_start(args, n);
+
+	for(i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+		printf("%d", num);
+		if (separator != NULL && i < n -1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
