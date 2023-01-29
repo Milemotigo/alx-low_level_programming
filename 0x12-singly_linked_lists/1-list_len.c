164dc201a0ccc802d7126f4cf25d08adcf738aa7
Milemotigo <global-config@mtgo3617@gmail.com>
2023-01-29 03:30:21 -0800

    list_t

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - a function that return the number of elements in a linked list_t
+ * @h: pointer to the linked list_t
+ * Return: len number of list_t
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h == NULL)
+		{
+			printf("[%d]\n", h->len);
+		}
+		count++;
+		h=h->next;
+	}
+	return (count);
+}
