355e9c6626db86e1e06eefd6eff87518d3ed6265
Milemotigo <global-config@mtgo3617@gmail.com>
2023-01-31 01:21:33 -0800

    peace

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,8 +4,11 @@
 #include "lists.h"
 
 /**
- * add_nodeint - a function that adds a new 
+ * add_nodeint - a function that adds a new
  * node at the beginning of a listint_t list
+ * @head: pointed to by a pointer to the head
+ * @n: value stored in the new node
+ * Return: Address of the new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
