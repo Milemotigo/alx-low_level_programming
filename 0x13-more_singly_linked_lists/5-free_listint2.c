f0d16489c72c09cb6dd6fd2da7cbd3f63f65c7eb
Milemotigo <global-config@mtgo3617@gmail.com>
2023-01-31 14:37:35 -0800

    free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list.
+ * @head: pointer to pointer to head
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *temp;
+
+	if (*head != NULL)
+	{
+	current = *head;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+		*head = NULL;
+	}
+	}
+}
