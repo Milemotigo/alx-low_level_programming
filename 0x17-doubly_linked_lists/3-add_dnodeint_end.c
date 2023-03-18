499e93c2c5666245a96c7b4452eab8374bbda423
Milemotigo <global-config@mtgo3617@gmail.com>
2023-03-18 06:41:45 -0700

    node@end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - function that adds a new node at the
+ * end of a dlistint_t list
+ * @n: integer value in the node
+ * @head: Double pointer to the head node
+ *
+ * Return: Address of the node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *temp;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		new->prev = NULL;
+		*head = new;
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	new->prev = temp;
+	return (new);
+}
+
