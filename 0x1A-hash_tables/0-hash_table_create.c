434cf9cd0cafafe439574a5cf447bb1c342c5487
milemotigo <mtigo3617@gmail.com>
2023-04-11 02:29:05 +0100

    open-to-work

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,15 @@
 #include "hash_tables.h"
 #include <stdlib.h>
-/** 
+/**
  * hash_table_t - a function that creates a hash tables
  * @size: size of the array
- *
- * @Return: The new hash table
+ * 
+ * @Return: Return a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
+
 	unsigned long int i = 0;
 
 	/* allocate a memory for the newly created hash table*/
