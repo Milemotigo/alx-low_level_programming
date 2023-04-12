0df369e97c899b040f4328f49a8bb7434e73074f
milemotigo <mtigo3617@gmail.com>
2023-04-12 11:06:33 +0100

    delete-a-node

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,21 +1,27 @@
 #include "hash_tables.h"
-
 /**
+ * hash_table_print: prints a hash table in a format
  *
- *
- *
+ * ht: hash table to be printed
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_table_t *node;
+	hash_node_t *node;
+	unsigned long int i = 0, speci = 0;
 
-	if (ht == NULL)
-		return (NULL);
-	ht = node;
-	while (node != NULL)
+	/*check if the table exist*/
+	if (ht == 0)
+		return;
+	printf("{");
+	for(node = ht->array[i]; node != NULL; node = node->next)
 	{
-		printf("%s", node);
-		node = node->next;
+		if (speci == 1)
+		{
+			printf(",");
+		}
+		printf("'%s':'%s'", node->key, node->value);
+		speci = 1;
 	}
+	printf("}\n");
 }
-
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete: a function that delete a hash table
+ *
+ * ht: hash table to delete
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i = 0;
+
+	if (ht == NULL)
+		return;
+	node = ht->array[i];
+	for(i = 0; i < ht->size; i++)
+	{
+		while(node != NULL)
+		{
+			/*copy the node*/
+			temp = node;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			node = node->next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
