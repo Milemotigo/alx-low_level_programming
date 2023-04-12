3472e413815b43ff913b2070163c05b958a9548a
milemotigo <mtigo3617@gmail.com>
2023-04-12 14:34:05 -0700

    create-ht

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,37 +1,29 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+
 /**
- * hash_table_t - a function that creates a hash tables
- * @size: size of the array
- * 
- * @Return: Return a pointer to the newly created hash table
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
-
 	unsigned long int i = 0;
 
 	if (size == 0)
-		return NULL;
-
-	/* allocate a memory for the newly created hash table*/
+		return (NULL);
 	hash_t = malloc(sizeof(hash_table_t));
-	/* check if memory was not allocate*/
-	if (hash_t == 0)
-	return NULL;
-	/* create an empty array that can hold element on the hash table */
+	if (hash_t == NULL)
+	return (NULL);
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
-	/* check if memory allocation for for array of hf failed*/
 	if (hash_t->array == NULL)
-{
-	return(NULL);
-}
-	/* Now we need to check if all the compartment(or bit) of the array is empty */
-	while(i < size)
+	return (NULL);
+	while (i < size)
 	hash_t->array[i] = NULL;
 	i++;
 	/*check if we are not out of the  memory*/
 	hash_t->size = size;
-	return(hash_t);
+	return (hash_t);
 }
