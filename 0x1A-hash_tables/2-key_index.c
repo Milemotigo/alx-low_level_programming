c11ca086d5396ba0b4583080007618c118a60219
milemotigo <mtigo3617@gmail.com>
2023-04-12 14:54:17 -0700

    djb2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,10 +2,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index: a function that helps us to find the index
- *
- *
- *
+ * key_index - a function that helps us to find the index
+ * @key: name of the element
+ * @size: size of the array
+ * Return: index
+ * Discription: Use the djb2 function
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
