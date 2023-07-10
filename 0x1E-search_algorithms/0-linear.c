c1cea27bd705c9cc5b8d2e8b18fec43d75380d03
milemotigo <mtigo@gmail.com>
2023-07-10 16:37:24 -0700

    task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t n;
 
+	if (array == NULL)
+		return (-1);
+
 	for (n = 0; n < size; n++)
 	{
 		printf("Value checked array [%lu] = [%d]\n", n, array[n]);
 		if (array[n] == value)
 			return (n);
-		if (array == NULL)
-			return (-1);
 	}
 	return (-1);
 }
